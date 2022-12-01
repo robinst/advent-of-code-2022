d4724fe2d0e16c0984ea58aaf90f04f16f89a6cd
Robin Stocker <robin@nibor.org>
2022-12-01 16:20:09 +1100

    Day 1

--- src/test/java/Day01.java
+++ src/test/java/Day01.java
@@ -1,23 +1,59 @@
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.Comparator;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class Day01 {
 
     public static long solve(String input) {
-        return input.lines().count();
+        var elves = input.split("\n\n");
+        return Arrays.stream(elves)
+                .map(elf -> elf.lines()
+                        .mapToLong(Long::parseLong)
+                        .sum())
+                .max(Long::compareTo)
+                .get();
+    }
+
+    public static long solve2(String input) {
+        var elves = input.split("\n\n");
+        return Arrays.stream(elves)
+                .map(elf -> elf.lines()
+                        .mapToLong(Long::parseLong)
+                        .sum())
+                .sorted(Comparator.reverseOrder())
+                .limit(3)
+                .mapToLong(Long::longValue)
+                .sum();
     }
 
     public static void main(String[] args) throws Exception {
         var input = Resources.readString(Resources.class.getResource("/day01.txt"));
         System.out.println(solve(input));
+        System.out.println(solve2(input));
     }
 
     @Test
-    void foo() {
-        assertEquals(2, solve("""
-                test
-                foo
-                """));
+    void example() {
+        final String input = """
+                1000
+                2000
+                3000
+
+                4000
+
+                5000
+                6000
+
+                7000
+                8000
+                9000
+
+                10000
+                """;
+        assertEquals(24000, solve(input));
+        assertEquals(45000, solve2(input));
     }
 }
